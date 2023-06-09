e455b4ba8b600254e8efd89de96b93c287bdaf3f
Alec Inthalangsy <19725614+alecji@users.noreply.github.com>
2023-06-09 01:13:10 -0400

    add error state

--- client/src/components/markdownToHtml/MarkdownToHtml.jsx
+++ client/src/components/markdownToHtml/MarkdownToHtml.jsx
@@ -6,30 +6,39 @@ import './MarkdownToHtml.css'
 const MarkdownToHtml = () => {
     const [markdown, setMarkdown] = useState('');
     const [html, setHtml] = useState('');
+    const [error, setError] = useState(null);
 
     const getHTML = async () => {
-        const rawResponse = await convertToHTML(markdown);
-        setHtml(rawResponse.html)
+        try {
+           const htmlResponse = await convertToHTML(markdown);
+            setHtml(htmlResponse)  
+            setError(null)
+        } catch (error) {
+            setError(error.message)
+        }
     };
 
     return (
         <>
             <section className='convert'>
                 <textarea
+                    data-testid='enterMarkdownTextBox'
                     value={markdown}
                     onChange={e => setMarkdown(e.target.value)}
                     rows={10}
                     cols={50}
                     placeholder="Enter markdown here"
                 />
-                <button onClick={getHTML}>Convert</button>
+                <button data-testid='convert-btn' onClick={getHTML}>Convert</button>
                 <textarea
+                    data-testid='html-conversion-box'
                     value={html}
                     rows={10}
                     cols={50}
                     readOnly
                     placeholder="HTML conversion"
                 />
+                 {error && <p>{error}</p>}
             </section>
             <div dangerouslySetInnerHTML={{ __html: html }}></div>
         </>
